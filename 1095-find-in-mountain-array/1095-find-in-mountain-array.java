db7714d46d3346f11e7c3c52a0b7aa726a9e3ad5
Soumya Roy <110768635+SoumyaRoy311000@users.noreply.github.com>
2022-10-09 13:45:22 +0530

    Time: 1 ms (38.66%), Space: 43.8 MB (37.98%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,75 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int peak = peakIndexInMountainArray(mountainArr);
+        int ans = binarySearch(target, mountainArr, peak, true);
+        if(ans==-1){
+            ans = binarySearch(target, mountainArr, peak, false);
+        }
+        return ans;
+    }
+    
+    int peakIndexInMountainArray(MountainArray arr){
+        int start = 0;
+        int end = arr.length()-1;
+        while(start<end){
+            int mid = start+(end-start)/2;
+            if(arr.get(mid)>arr.get(mid+1)){
+                end = mid;
+            }
+            else if(arr.get(mid)<arr.get(mid+1)){
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+    
+    int binarySearch(int target, MountainArray arr, int peak, boolean isAsc){
+        if(isAsc){
+            int start = 0;
+            int end = peak;
+            int ans = -1;
+            while(start<=end){
+                int mid = start+(end-start)/2;
+                if(arr.get(mid)<target){
+                    start = mid+1;
+                }
+                else if(arr.get(mid)>target){
+                    end = mid-1;
+                }
+                else{
+                    ans = mid;
+                    end = mid-1;
+                }
+            }
+            return ans;
+        }
+        else{
+            int start = peak;
+            int end = arr.length()-1;
+            int ans = -1;
+            while(start<=end){
+                int mid = start+(end-start)/2;
+                if(arr.get(mid)>target){
+                    start = mid+1;
+                }
+                else if(arr.get(mid)<target){
+                    end = mid-1;
+                }
+                else{
+                    ans = mid;
+                    end = mid-1;
+                }
+            }
+            return ans;
+        }
+    }
+}
