09b65febd9a30a4041ae91e19ebd37ab710fac0c
Soumya Roy <110768635+SoumyaRoy311000@users.noreply.github.com>
2022-10-18 17:36:16 +0530

    Time: 4 ms (36.12%), Space: 42.8 MB (12.19%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -1,32 +1,32 @@
 class Solution {
     public int oddCells(int m, int n, int[][] indices) {
-        int[][] matrix = new int[m][n];
-        for(int[] increment: indices){
-            function(matrix, increment);
-        }
-        return countOdd(matrix);
-    }
-    
-    void function(int[][] matrix, int[] increment){
-        for(int i = 0; i<matrix.length; i++){
-            for(int j = 0; j<matrix[i].length; j++){
-                if(increment[0] == i){
-                    matrix[i][j]+=1;
+        int count = 0;
+        boolean[] isOdd = new boolean[m*n];
+        for(int[] increments: indices){
+            int elementstartrows = (increments[0])*n;
+            int elementendrows = elementstartrows+n-1;
+            for(int i = elementstartrows; i<= elementendrows; i++){
+                if(isOdd[i]){
+                    isOdd[i] = false;
                 }
-                if(increment[1]==j){
-                    matrix[i][j]+=1;
+                else{
+                    isOdd[i] = true;
                 }
             }
-        }
-    }
-    
-    int countOdd(int[][] matrix){
-        int count = 0;
-        for(int i = 0; i<matrix.length; i++){
-            for(int j = 0; j<matrix[i].length; j++){
-                if(matrix[i][j]%2==1){
-                    count++;
+            int elementcolumn = increments[1];
+            while(elementcolumn<(m*n)){
+                if(isOdd[elementcolumn]){
+                    isOdd[elementcolumn] = false;
                 }
+                else{
+                    isOdd[elementcolumn] = true;
+                }
+                elementcolumn+=n;
+            }
+        }
+        for(int i = 0; i<isOdd.length; i++){
+            if(isOdd[i]){
+                count++;
             }
         }
         return count;
