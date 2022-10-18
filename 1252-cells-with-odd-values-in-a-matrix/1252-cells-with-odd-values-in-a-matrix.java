3fcdd629739d795b229be53c6fe21ba3492851e4
Soumya Roy <110768635+SoumyaRoy311000@users.noreply.github.com>
2022-10-18 17:53:05 +0530

    Time: 18 ms (5.67%), Space: 42.2 MB (49.39%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -3,20 +3,22 @@ class Solution {
         int count = 0;
         boolean[] isOdd = new boolean[m*n];
         for(int[] increments: indices){
-            for(int i = increments[0]*n; i< increments[0]*n+n; i++){
-                if(isOdd[i]){
+            for(int i = 0; i<isOdd.length; i++){
+                if(i >= increments[0]*n && i< increments[0]*n+n){
+                    if(isOdd[i]){
                     isOdd[i] = false;
+                    }
+                    else{
+                        isOdd[i] = true;
+                    }
                 }
-                else{
-                    isOdd[i] = true;
-                }
-            }
-            for(int i= increments[1]; i<isOdd.length; i=i+n){
-                if(isOdd[i]){
+                if(i%n == increments[1]){
+                    if(isOdd[i]){
                     isOdd[i] = false;
-                }
-                else{
-                    isOdd[i] = true;
+                    }
+                    else{
+                        isOdd[i] = true;
+                    }
                 }
             }
         }
