cc11f71fcbcb5250b80abdee7134fbb29ca4a087
Soumya Roy <110768635+SoumyaRoy311000@users.noreply.github.com>
2022-10-18 18:05:02 +0530

    Time: 14 ms (5.67%), Space: 42.2 MB (49.39%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -3,37 +3,25 @@ class Solution {
         int count = 0;
         boolean[] isOdd = new boolean[m*n];
         for(int[] increments: indices){
-            count = getCount(n, count, isOdd, increments);
-        }
-        return count;
-    }
-
-    int getCount(int n, int count, boolean[] isOdd, int[] increments) {
-        for(int i = 0; i< isOdd.length; i++){
-            count = getCount(n, count, isOdd, increments, i);
-        }
-        return count;
-    }
-
-    int getCount(int n, int count, boolean[] isOdd, int[] increments, int i) {
-        if(i >= increments[0]* n && i < increments[0]* n + n){
-            if(isOdd[i]){
-                isOdd[i] = false;
-                count--;
+            for(int i = increments[0]*n; i< increments[0]*n+n; i++){
+                if(isOdd[i]){
+                    isOdd[i] = false;
+                    count--;
+                }
+                else{
+                    isOdd[i] = true;
+                    count++;
+                }
             }
-            else{
-                isOdd[i] = true;
-                count++;
-            }
-        }
-        if(i % n == increments[1]){
-            if(isOdd[i]){
-                isOdd[i] = false;
-                count--;
-            }
-            else{
-                isOdd[i] = true;
-                count++;
+            for(int i= increments[1]; i<isOdd.length; i=i+n){
+                if(isOdd[i]){
+                    isOdd[i] = false;
+                    count--;
+                }
+                else{
+                    isOdd[i] = true;
+                    count++;
+                }
             }
         }
         return count;
