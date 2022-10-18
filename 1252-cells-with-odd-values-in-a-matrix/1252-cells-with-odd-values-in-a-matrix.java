ff9b0e4ab6cc4f9cc412cbe7ea48bdb7444d4d68
Soumya Roy <110768635+SoumyaRoy311000@users.noreply.github.com>
2022-10-18 18:04:35 +0530

    Time: 24 ms (5.67%), Space: 42.5 MB (28.14%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -10,25 +10,30 @@ class Solution {
 
     int getCount(int n, int count, boolean[] isOdd, int[] increments) {
         for(int i = 0; i< isOdd.length; i++){
-            if(i >= increments[0]* n && i< increments[0]* n + n){
-                if(isOdd[i]){
-                    isOdd[i] = false;
-                    count--;
-                }
-                else{
-                    isOdd[i] = true;
-                    count++;
-                }
+            count = getCount(n, count, isOdd, increments, i);
+        }
+        return count;
+    }
+
+    int getCount(int n, int count, boolean[] isOdd, int[] increments, int i) {
+        if(i >= increments[0]* n && i < increments[0]* n + n){
+            if(isOdd[i]){
+                isOdd[i] = false;
+                count--;
             }
-            if(i% n == increments[1]){
-                if(isOdd[i]){
-                    isOdd[i] = false;
-                    count--;
-                }
-                else{
-                    isOdd[i] = true;
-                    count++;
-                }
+            else{
+                isOdd[i] = true;
+                count++;
+            }
+        }
+        if(i % n == increments[1]){
+            if(isOdd[i]){
+                isOdd[i] = false;
+                count--;
+            }
+            else{
+                isOdd[i] = true;
+                count++;
             }
         }
         return count;
